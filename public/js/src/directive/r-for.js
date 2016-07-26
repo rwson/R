0ab7809dc01d243db78c01d527bbe1d52e573ed6
rwson <rw_Song@sina.com>
2016-07-26 17:52:52 +0800

    fixed the bug with directive "r-for" that one tag bind two or more diretives

--- public/js/src/directive/r-for.js
+++ public/js/src/directive/r-for.js
@@ -58,12 +58,11 @@
 
                             if (childDirs && childDir.length) {
 
-                                //  遍历当前子元素的指令
-                                childDirs.forEach(function (cDir) {
 
-                                    //  同样保存当前子元素的一个副本
-                                    childEl = dir.el.cloneNode(true);
+                                //  同样保存当前子元素的一个副本
+                                childEl = dir.el.cloneNode(true);
 
+                                if (childDirs.length === 1) {
                                     //  遍历子节点
                                     Dom.loopChild(elCloned, function (child, lowest) {
                                         if (Dom.compareNodes(child, childEl)) {
@@ -76,23 +75,59 @@
 
                                             loopParent = child.parentNode;
 
-                                            //  实例化指令
-                                            tDir = new cDir.directive(dir);
+                                            //  遍历当前子元素的指令
+                                            childDirs.forEach(function (cDir) {
+                                                //  实例化指令
+                                                tDir = new cDir.directive(dir);
 
-                                            //  取得指令绑定是属性值
-                                            expArr = cDir.exp.split(".").slice(1);
-                                            value = scope.exec(cDir.exp) || scope.execDeep(inExp, expArr);
+                                                //  取得指令绑定是属性值
+                                                expArr = cDir.exp.split(".").slice(1);
+                                                value = scope.exec(cDir.exp) || scope.execDeep(inExp, expArr);
 
-                                            //  判断当前指令类型,事件类型就修改回调函数里面this指向
-                                            if (cDir.dirType === "event") {
-                                                tDir.link(child, value, this.scope, inExp);
-                                            } else {
-                                                tDir.link(child, value, this.scope);
-                                            }
+                                                //  判断当前指令类型,事件类型就修改回调函数里面this指向
+                                                if (cDir.dirType === "event") {
+                                                    tDir.link(child, value, this.scope, inExp);
+                                                } else {
+                                                    tDir.link(child, value, this.scope);
+                                                }
+                                            }, this);
+
                                             loopParent.replaceChild(child, child);
                                         }
                                     }, this);
-                                }, this);
+                                } else {
+                                    //  遍历子节点
+                                    Dom.loopChild(elCloned, function (child, lowest) {
+                                        if (Dom.compareNodes(child, childEl)) {
+                                            rid = Tool.randomStr();
+
+                                            //  重新给相关子元素设置rid属性
+                                            Dom.setAttributes(child, {
+                                                "rid": rid
+                                            });
+
+                                            loopParent = child.parentNode;
+
+                                            childDirs.forEach(function (cDir) {
+                                                //  实例化指令
+                                                tDir = new cDir.directive(dir);
+
+                                                //  取得指令绑定是属性值
+                                                expArr = cDir.exp.split(".").slice(1);
+                                                value = scope.exec(cDir.exp) || scope.execDeep(inExp, expArr);
+
+                                                //  判断当前指令类型,事件类型就修改回调函数里面this指向
+                                                if (cDir.dirType === "event") {
+                                                    tDir.link(child, value, this.scope, inExp);
+                                                } else {
+                                                    tDir.link(child, value, this.scope);
+                                                }
+                                            }, this);
+
+                                            loopParent.replaceChild(child, child);
+                                        }
+                                    }, this);
+                                }
                             }
                         }, this);
                     }
